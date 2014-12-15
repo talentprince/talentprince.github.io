aa464355eb3da66cd7cd69774f08ad5f963f520b
Chen Chen <chen.chen@rea-group.com>
2014-12-15 15:56:21 +0800

    refine source

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/blog/"
+http_images_path = "/blog/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/blog/fonts"
+css_dir = "public/blog/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
